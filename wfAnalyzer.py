23b209e95541b48f45df5f1480210e176a7f19a0
Ale0x78 <anahape@ncsu.edu>
2023-02-01 00:57:02 +0000

    Moved where the API requests are!

--- wfAnalyzer.py
+++ wfAnalyzer.py
@@ -9,6 +9,7 @@ import os
 import time
 from datetime import datetime
 
+
 class critical_gh_context(Enum):
     ACTOR = "github.actor"
     PULL_REQUEST_BODY = "github.event.pull_request.body"
@@ -22,21 +23,23 @@ class critical_gh_context(Enum):
 class critical_secrets(Enum):
     SECRET_CDD = "secrets."
 
+
 class critical_permissions(Enum):
     ONLY_WF_DECLARATION = "Permissions declaration at workflow level"
     NO_DECLARATION = "Permissions are not declared in the workflow"
     PERMS_DISCREPANCY = "Permissions do not match the required ones"
+
 
 class critical_tp_workflow(Enum):
     WF_OOD = "Workflow out of date"
     NO_PINNING = "Workflow not pinned at commit"
 
 
-
 def get_action_intel(action):
+    print(f"Getting data for {action}")
     TOKEN = os.getenv("ght", "NO_TOKEN")
     with MongoClient() as client:
-        res = client['ghast']['cache'].find_one({"name" : action})
+        res = client["ghast"]["cache"].find_one({"name": action})
         if res:
             return res
         else:
@@ -44,81 +47,94 @@ def get_action_intel(action):
             req = requests.get(api_url, headers={"Authorization": f"token {TOKEN}"})
             if req.status_code == 200:
                 res = json.loads(req.text)
-                res['name'] = action
-                client['ghast']['cache'].insert_one(res)
+                res["name"] = action
+                client["ghast"]["cache"].insert_one(res)
                 return res
             else:
                 await_limit()
                 return get_action_intel(action)
 
+
 def await_limit():
     limit, until = get_status()
     if limit < 0:
-        time.sleep(until - int(datetime.utcnow().timestamp()) + 10)
+        delta = until - int(datetime.utcnow().timestamp()) + 10
+        print(f"Github API limit reached, waiting {delta} seconds")
+        time.sleep(delta)
+
 
 def get_status():
     TOKEN = os.getenv("ght", "NO_TOKEN")
     api_url = "https://api.github.com/rate_limit"
     req = requests.get(api_url, headers={"Authorization": f"token {TOKEN}"})
     try:
-        return req.json().get('resources', {}).get("core", {}).get("remaining", 0), req.json().get('resources', {}).get("core", {}).get("reset", 60 * 60)
+        return req.json().get("resources", {}).get("core", {}).get(
+            "remaining", 0
+        ), req.json().get("resources", {}).get("core", {}).get("reset", 60 * 60)
     except:
         return 0, 60 * 60
+
 
 def getOODWf(wf):
     trueCount = 0
     falseCount = 0
     ret = []
-    for j in wf['jobs']:
-        for s in wf['jobs'][j]['steps']:
-            sec = s.get('security', None)
+    for j in wf["jobs"]:
+        for s in wf["jobs"][j]["steps"]:
+            sec = s.get("security", None)
             if sec:
-                if sec.get('TP Actions Up-to-date') != None:
-                    if sec.get('TP Actions Up-to-date'):
+                if sec.get("TP Actions Up-to-date") != None:
+                    if sec.get("TP Actions Up-to-date"):
                         trueCount += 1
                     else:
                         falseCount += 1
 
     return trueCount, falseCount
 
+
 def getOOD(wf):
     out_date = []
     ret = []
-    for j in wf['jobs']:
-        for s in wf['jobs'][j]['steps']:
-            sec = s.get('security', None)
+    for j in wf["jobs"]:
+        for s in wf["jobs"][j]["steps"]:
+            sec = s.get("security", None)
             if sec:
-                if sec.get('TP Actions Up-to-date') != None:
-                    out_date.append((j, s.get('uses'), sec.get('TP Actions Up-to-date')))
+                if sec.get("TP Actions Up-to-date") != None:
+                    out_date.append(
+                        (j, s.get("uses"), sec.get("TP Actions Up-to-date"))
+                    )
 
     return out_date
 
+
 def getRuns(wf):
     _runs = []
-    for j in wf['jobs']:
-        for s in wf['jobs'][j]['steps']:
-            sec = s.get('security')
-            if sec.get('runs'):
-                _runs.append(sec.get('runs'))
+    for j in wf["jobs"]:
+        for s in wf["jobs"][j]["steps"]:
+            sec = s.get("security")
+            if sec.get("runs"):
+                _runs.append(sec.get("runs"))
     return _runs
-                
+
 
 def getUses(repo):
     _uses = []
     _improper_use = []
     for wf in repo:
-        for j in wf['jobs']:
-            for s in wf['jobs'][j]['steps']:
-                uses = s.get('uses', None)
+        for j in wf["jobs"]:
+            for s in wf["jobs"][j]["steps"]:
+                uses = s.get("uses", None)
                 _uses.append(uses)
     return _uses
 
+
 def getPerms(wf):
     _perms = {}
-    _perms.update(wf=wf.get('permissions', None), jobs={})
-    for j in wf['jobs']:
-        _perms['jobs'].update({j: wf['jobs'][j].get('permissions')})
+    _perms.update(wf=wf.get("permissions", None), jobs={})
+    for j in wf["jobs"]:
+        _perms["jobs"].update({j: wf["jobs"][j].get("permissions")})
     return _perms
+
 
 def main(args):
     dictwf = pickle.load(open(f"{args.source}/savedDictWfs.dat", "rb"))
@@ -131,54 +147,106 @@ def main(args):
             _runs = getRuns(wf)
             _uses = getOOD(wf)
             _perms = getPerms(wf)
-            if isinstance(wf.get('events'), dict):
-                vulns[wf_file].update({wf.get('name'): {'events': (wf.get('events').get('type'), wf.get('events').get('security_rank')), "issues": []}})
+            if isinstance(wf.get("events"), dict):
+                vulns[wf_file].update(
+                    {
+                        wf.get("name"): {
+                            "events": (
+                                wf.get("events").get("type"),
+                                wf.get("events").get("security_rank"),
+                            ),
+                            "issues": [],
+                        }
+                    }
+                )
             else:
-                for e in wf.get('events'):
-                    vulns[wf_file].update({wf.get('name'): {'events': (e.get('type'), e.get('security_rank')), "issues": []}})
-                    
+                for e in wf.get("events"):
+                    vulns[wf_file].update(
+                        {
+                            wf.get("name"): {
+                                "events": (e.get("type"), e.get("security_rank")),
+                                "issues": [],
+                            }
+                        }
+                    )
+
             if _runs != []:
                 if _runs:
-                    vulns[wf_file][wf.get('name')]['issues'].append(([i.name for i in critical_gh_context if i.value in ''.join(_runs[0][0]['line'])], ''.join(_runs[0][0]['line'])))
-                    vulns[wf_file][wf.get('name')]['issues'].append(([i.name for i in critical_secrets if i.value in ''.join(_runs[0][0]['line'])], ''.join(_runs[0][0]['line'])))
-            
-            
+                    vulns[wf_file][wf.get("name")]["issues"].append(
+                        (
+                            [
+                                i.name
+                                for i in critical_gh_context
+                                if i.value in "".join(_runs[0][0]["line"])
+                            ],
+                            "".join(_runs[0][0]["line"]),
+                        )
+                    )
+                    vulns[wf_file][wf.get("name")]["issues"].append(
+                        (
+                            [
+                                i.name
+                                for i in critical_secrets
+                                if i.value in "".join(_runs[0][0]["line"])
+                            ],
+                            "".join(_runs[0][0]["line"]),
+                        )
+                    )
+
             if len(_perms) > 0:
                 wf_perms = False
-                if _perms.get('wf') != 'None':
+                if _perms.get("wf") != "None":
                     wf_perms = True
-                for job_name, job_p in _perms.get('jobs').items():
+                for job_name, job_p in _perms.get("jobs").items():
                     if not job_p and wf_perms:
-                        vulns[wf_file][wf.get('name')]['issues'].append((job_name, critical_permissions.ONLY_WF_DECLARATION.name))
+                        vulns[wf_file][wf.get("name")]["issues"].append(
+                            (job_name, critical_permissions.ONLY_WF_DECLARATION.name)
+                        )
                     elif not job_p and not wf_perms:
-                        vulns[wf_file][wf.get('name')]['issues'].append((job_name, critical_permissions.NO_DECLARATION.name))
+                        vulns[wf_file][wf.get("name")]["issues"].append(
+                            (job_name, critical_permissions.NO_DECLARATION.name)
+                        )
             api_url = "https://api.github.com/repos/" + wf_file + "/tags"
-            req = requests.get(api_url, headers={"Authorization": "token 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"})
+            req = requests.get(
+                api_url,
+                headers={
+                    "Authorization": "token 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"
+                },
+            )
             tags = json.loads(req.text)
             if len(_uses) > 0:
                 for ood in _uses:
                     if re.match(ood[1].split("@")[-1], commit_rex):
-                        if not (ood[1].split("@")[-1] == tags[0].get('commit').get('sha')):
+                        if not (
+                            ood[1].split("@")[-1] == tags[0].get("commit").get("sha")
+                        ):
                             print("CASE")
-                            vulns[wf_file][wf.get('name')]['issues'].append((ood[0], critical_tp_workflow.WF_OOD.name))
+                            vulns[wf_file][wf.get("name")]["issues"].append(
+                                (ood[0], critical_tp_workflow.WF_OOD.name)
+                            )
                     elif not ood[2] and not re.match(ood[1].split("@")[-1], commit_rex):
-                        vulns[wf_file][wf.get('name')]['issues'].append((ood[0], critical_tp_workflow.WF_OOD.name))
-                        vulns[wf_file][wf.get('name')]['issues'].append((ood[0], critical_tp_workflow.NO_PINNING.name))
+                        vulns[wf_file][wf.get("name")]["issues"].append(
+                            (ood[0], critical_tp_workflow.WF_OOD.name)
+                        )
+                        vulns[wf_file][wf.get("name")]["issues"].append(
+                            (ood[0], critical_tp_workflow.NO_PINNING.name)
+                        )
                     elif not ood[2] and re.match(ood[1].split("@")[-1], commit_rex):
-                        vulns[wf_file][wf.get('name')]['issues'].append((ood[0], critical_tp_workflow.WF_OOD.name))
+                        vulns[wf_file][wf.get("name")]["issues"].append(
+                            (ood[0], critical_tp_workflow.WF_OOD.name)
+                        )
                     elif not re.match(ood[1].split("@")[-1], commit_rex):
-                        vulns[wf_file][wf.get('name')]['issues'].append((ood[0], critical_tp_workflow.NO_PINNING.name))
+                        vulns[wf_file][wf.get("name")]["issues"].append(
+                            (ood[0], critical_tp_workflow.NO_PINNING.name)
+                        )
     with open(f"{args.dest}", "w") as f:
         f.write(json.dumps(vulns))
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument("--src", dest='source', type=str)
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("--src", dest="source", type=str)
     parser.add_argument("--dest", dest="dest", type=str)
 
     args = parser.parse_args()
     main(args)
-
-
-
-
--- wfExtractor.py
+++ wfExtractor.py
@@ -1,29 +1,20 @@
-import pickle
-
-import io
-
-import subprocess
 import os
-from tkinter import mainloop
-from pip import main
-import ruamel.yaml as yaml
-import ruamel
-# import yaml
 import re
+import ruamel.yaml as yaml
 from hashlib import sha256
-import requests
-from pprint import pprint
-import semantic_version
+import time
 import json
 import argparse
+import semantic_version
+import ruamel
+from requests import get
+from datetime import datetime
+from pymongo.mongo_client import MongoClient
 
-parser = argparse.ArgumentParser(description='')
-parser.add_argument("--wf", dest='workflowfile', type=str)
-parser.add_argument("--dest", dest="destination", type=str)
-
-args = parser.parse_args()
 # get position of string ": name:" in a file
 from typing import Dict, List
+
+keys = os.getenv("ght", "NO_TOKEN").split("|")
 
 event_rank = {
     "fork": 3,
@@ -43,13 +34,64 @@ event_rank = {
 }
 
 
+def get_key() -> str:
+    curr = keys.pop(0)
+    keys.append(curr)
+    return curr
+
+
+def get_tags(repo):
+    print(f"Getting tags for {repo}")
+    with MongoClient() as client:
+        item = client["ghast"]["repo_cache"].find_one({"repo": repo})
+        if item:
+            return item["tags"]
+        api_url = "https://api.github.com/repos/" + repo + "/tags"
+        req = get(api_url, headers={"Authorization": f"token {get_key()}"})
+        if req.status_code == 200:
+            tags = json.loads(req.text)
+            entry = {"repo": repo, "tags": tags}
+            client["ghast"]["repo_cache"].insert_one(entry)
+        if req.status_code != 200:
+            print("Error getting tags", req.status_code, req.text)
+            await_limit()
+            return get_tags(repo)
+    tags = json.loads(req.text)
+    return tags
+
+
+def get_action_intel(action):
+    print(f"Getting data for the action {action}")
+    with MongoClient() as client:
+        res = client["ghast"]["cache"].find_one({"name": action})
+        if res:
+            return res
+        else:
+            api_url = "https://api.github.com/repos/" + action + "/releases/latest"
+
+            req = get(
+                api_url,
+                headers={"Authorization": f"token {get_key()}"},
+            )
+            if req.status_code == 200:
+                res = json.loads(req.text)
+                res["name"] = action
+                client["ghast"]["cache"].insert_one(res)
+                return res
+            else:
+                print(f"Waiting for rate limit... {req.status_code}, {req.content}")
+                await_limit()
+                return get_action_intel(action)
+
+
 def get_position(file_name, string):
     indexes = []
-    with open(file_name, 'r') as f:
+    with open(file_name, "r") as f:
         for i, line in enumerate(f):
             if string in line:
                 indexes.append(i + 1)
     return indexes
+
 
 # Separate a string based on a patter similar to "#example\nname: example"
 def separate_string(string):
@@ -58,7 +100,9 @@ def separate_string(string):
     print(found)
     indx = []
     for e, i in enumerate(found):
-        indx.append((string.find(i), string.find(found[e + 1]) if e + 1 < len(found) else -1))
+        indx.append(
+            (string.find(i), string.find(found[e + 1]) if e + 1 < len(found) else -1)
+        )
     return indx
 
 
@@ -74,29 +118,46 @@ def extract_workflow(sample):
         return output
 
     try:
-        output['name'] = workflow.get('name')
+        output["name"] = workflow.get("name")
 
-        output['permissions'] = repr(workflow.get('permissions'))
-        output['conditional'] = workflow.get('if')
-    
-        if isinstance(workflow['on'], str):
-            output['events'] = {"type": workflow.get('on'), "security_rank": event_rank[workflow.get('on')]}
-        elif isinstance(workflow['on'], list):
-            output['events'] = [{"type": workflow.get('on')[i], "security_rank": event_rank[workflow.get('on')[i]]} for i in
-                                range(len(workflow['on']))]
-        elif isinstance(workflow['on'], dict):
-            output['events'] = []
-            for event in workflow.get('on'):
-                if isinstance(workflow.get('on')[event], dict):
-                    output['events'].append({"type": event, "security_rank": event_rank[event], "filters": [k for k in workflow.get('on')[event].keys()]})
+        output["permissions"] = repr(workflow.get("permissions"))
+        output["conditional"] = workflow.get("if")
+
+        if isinstance(workflow["on"], str):
+            output["events"] = {
+                "type": workflow.get("on"),
+                "security_rank": event_rank[workflow.get("on")],
+            }
+        elif isinstance(workflow["on"], list):
+            output["events"] = [
+                {
+                    "type": workflow.get("on")[i],
+                    "security_rank": event_rank[workflow.get("on")[i]],
+                }
+                for i in range(len(workflow["on"]))
+            ]
+        elif isinstance(workflow["on"], dict):
+            output["events"] = []
+            for event in workflow.get("on"):
+                if isinstance(workflow.get("on")[event], dict):
+                    output["events"].append(
+                        {
+                            "type": event,
+                            "security_rank": event_rank[event],
+                            "filters": [k for k in workflow.get("on")[event].keys()],
+                        }
+                    )
                 else:
-                    output['events'].append({"type": event, "security_rank": event_rank[event]})
+                    output["events"].append(
+                        {"type": event, "security_rank": event_rank[event]}
+                    )
         else:
-            assert False, f'Unsupported type {type(workflow.get("on"))} for workflow.on field'
+            assert (
+                False
+            ), f'Unsupported type {type(workflow.get("on"))} for workflow.on field'
 
-   
-        jobs = workflow.get('jobs', dict())
-        output['jobs'] = extract_jobs(jobs, True if workflow.get('if') else False)
+        jobs = workflow.get("jobs", dict())
+        output["jobs"] = extract_jobs(jobs, True if workflow.get("if") else False)
     except:
         return output
     return output
@@ -108,11 +169,13 @@ def extract_jobs(jobs, conditional_wf):
     for id, job in jobs.items():
         output[id] = dict()
 
-        output[id]['name'] = job.get('name')
-        output[id]['uses'] = job.get('uses')
-        output[id]['conditional'] = job.get('if')
-        output[id]['permissions'] = job.get('permissions')
-        output[id]['steps'] = extract_steps(job.get('steps', []), True if job.get('if') else False, conditional_wf)
+        output[id]["name"] = job.get("name")
+        output[id]["uses"] = job.get("uses")
+        output[id]["conditional"] = job.get("if")
+        output[id]["permissions"] = job.get("permissions")
+        output[id]["steps"] = extract_steps(
+            job.get("steps", []), True if job.get("if") else False, conditional_wf
+        )
 
     return output
 
@@ -123,21 +186,25 @@ def extract_steps(steps, conditional_job, conditional_wf):
     for i, step in enumerate(steps):
         item = dict()
 
-        item['name'] = step.get('name')
-        item['conditional'] = step.get('if')
-        item['position'] = i + 1
-        item['uses'] = step.get('uses', None)
-        _run = step.get('run', None)
-        item['security'] = {}
-        if step.get('uses', None):
-            item['security'].update({"TP Actions Up-to-date": check_uses_version(item['uses'])})
+        item["name"] = step.get("name")
+        item["conditional"] = step.get("if")
+        item["position"] = i + 1
+        item["uses"] = step.get("uses", None)
+        _run = step.get("run", None)
+        item["security"] = {}
+        if step.get("uses", None):
+            item["security"].update(
+                {"TP Actions Up-to-date": check_uses_version(item["uses"])}
+            )
             # print(item['security'])
         if _run is not None:
-            item['run'] = len(_run.split('\n'))
-            item['run_hash'] = sha256(str.encode(_run)).hexdigest()
-            item['security'] = {"runs": run_analyzer(step, conditional_wf, conditional_job)}
+            item["run"] = len(_run.split("\n"))
+            item["run_hash"] = sha256(str.encode(_run)).hexdigest()
+            item["security"] = {
+                "runs": run_analyzer(step, conditional_wf, conditional_job)
+            }
         else:
-            item['run'] = 0
+            item["run"] = 0
         output.append(item)
 
     return output
@@ -145,20 +212,22 @@ def extract_steps(steps, conditional_job, conditional_wf):
 
 def perms_analyzer(wf):
     wf_name, wf_dict = wf
-    if wf_dict['permissions']:
+    if wf_dict["permissions"]:
         print(f"Workflow has permissions: {wf_dict['permissions']}")
-    for job in wf_dict['jobs'].keys():
-        if wf_dict['jobs'][job]['permissions']:
+    for job in wf_dict["jobs"].keys():
+        if wf_dict["jobs"][job]["permissions"]:
             print(f"Job {job} has permissions: {wf_dict['jobs'][job]['permissions']}")
 
 
-def run_analyzer(step: Dict[str, any], cond_wf: bool, cond_job: bool) -> List[Dict[str, any]]:
+def run_analyzer(
+    step: Dict[str, any], cond_wf: bool, cond_job: bool
+) -> List[Dict[str, any]]:
     rex = r".*(\${{\s*github\.).*"
     ret = []
-    if step['run']:
-        for i, l in enumerate(step['run'].split("\n")):
+    if step["run"]:
+        for i, l in enumerate(step["run"].split("\n")):
             if re.match(rex, l):
-                if step.get('if', None) or cond_wf or cond_job:
+                if step.get("if", None) or cond_wf or cond_job:
                     ret.append({"position": i, "line": l, "conditional": True})
                     # print(f"Conditional direct run at job {job} @ step {step['name']}")
                 else:
@@ -169,17 +238,19 @@ def run_analyzer(step: Dict[str, any], cond_wf: bool, cond_job: bool) -> List[Di
 
 def secret_analyzer(job: str, step: Dict[str, any]):
     secret_re = r".*(secrets\.).*"
-    if step['run']:
-        for l in step['run']:
+    if step["run"]:
+        for l in step["run"]:
             if re.match(secret_re, l):
                 print(f"Secret appears at job {job} @ step {step['name']}")
 
 
 def workflow_analyzer(wf):
     wf_name, wf_dict = wf
-    for job in wf_dict['jobs'].keys():
-        for step in wf_dict['jobs'][job]['steps']:
-            run_analyzer(job, step, wf_dict['conditional'], wf_dict['jobs'][job]['conditional'])
+    for job in wf_dict["jobs"].keys():
+        for step in wf_dict["jobs"][job]["steps"]:
+            run_analyzer(
+                job, step, wf_dict["conditional"], wf_dict["jobs"][job]["conditional"]
+            )
             secret_analyzer(job, step)
 
 
@@ -187,47 +258,61 @@ def check_uses_version(action: str) -> bool:
     if action.split("@")[-1].replace("v", "") in ["master", "main"]:
         return True
     try:
-        version = semantic_version.Version.coerce(action.split("@")[-1].replace("v", ""))
+        version = semantic_version.Version.coerce(
+            action.split("@")[-1].replace("v", "")
+        )
     except:
         version = action.split("@")[-1].replace("v", "")
     position = action.split("@")[0]
-    api_url = "https://api.github.com/repos/" + position + "/releases/latest"
-    req = requests.get(api_url, headers={"Authorization": "token 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"})
-    if req.status_code == 200:
-        data = json.loads(req.text)
-        latest_version = str(data['tag_name']).replace("v", "")
-        try:
-            latest_version = semantic_version.Version.coerce(latest_version)
-            if latest_version.major == version.major:
-                return True
-            else:
-                return version >= latest_version
-        except:
-            return latest_version == version
-    else:
-        return False
+    data = get_action_intel(position)
+    latest_version = str(data["tag_name"]).replace("v", "")
+    try:
+        latest_version = semantic_version.Version.coerce(latest_version)
+        if latest_version.major == version.major:
+            return True
+        else:
+            return version >= latest_version
+    except:
+        return latest_version == version
 
 
-if __name__ == "__main__":
-    wfs = []
-    print("Starting workflow analysis...")
-    with open(args.workflowfile, encoding='utf-8', errors="replace") as f:
-        dat = f.read()
-        for wf in dat.split("___WORKFLOW END___\n"):
-            # print(wf + "---------------------\n")
-            if wf != "":
-                wf_name = wf.split("\n")[0].replace("#", "")
-                if wf.find(u'\x04') > 0:
-                    print(wf[wf.find(u'\x04'):])
-                wfs.append((wf_name, extract_workflow(wf)))
-                print('\r' + str(len(wfs)))
-    wfs_dict = dict()
-    for w in wfs:
-        if 'jobs' not in w[1].keys():
-            continue
-        if w[0] not in wfs_dict.keys():
-            wfs_dict.update({w[0]: []})
-        wfs_dict[w[0]].append(w[1])
-    print(wfs_dict.keys())
-    with open(args.destination+"/savedDictWfs.dat", "wb") as f:
-        pickle.dump(wfs_dict, f)
+def get_status():
+    api_url = "https://api.github.com/rate_limit"
+    req = get(api_url, headers={"Authorization": "token f{TOKEN}"})
+    try:
+        return req.json().get("resources", {}).get("core", {}).get(
+            "remaining", 0
+        ), req.json().get("resources", {}).get("core", {}).get("reset", 60 * 60)
+    except:
+        return 0, 60 * 60
+
+
+def await_limit():
+    limit, until = get_status()
+    if limit < 0:
+        time.sleep(until - int(datetime.utcnow().timestamp()) + 10)
+
+
+# if __name__ == "__main__":
+#     wfs = []
+#     print("Starting workflow analysis...")
+#     with open(args.workflowfile, encoding='utf-8', errors="replace") as f:
+#         dat = f.read()
+#         for wf in dat.split("___WORKFLOW END___\n"):
+#             # print(wf + "---------------------\n")
+#             if wf != "":
+#                 wf_name = wf.split("\n")[0].replace("#", "")
+#                 if wf.find(u'\x04') > 0:
+#                     print(wf[wf.find(u'\x04'):])
+#                 wfs.append((wf_name, extract_workflow(wf)))
+#                 print('\r' + str(len(wfs)))
+#     wfs_dict = dict()
+#     for w in wfs:
+#         if 'jobs' not in w[1].keys():
+#             continue
+#         if w[0] not in wfs_dict.keys():
+#             wfs_dict.update({w[0]: []})
+#         wfs_dict[w[0]].append(w[1])
+#     print(wfs_dict.keys())
+#     with open(args.destination+"/savedDictWfs.dat", "wb") as f:
+#         pickle.dump(wfs_dict, f)
